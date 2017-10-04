19b1a4c402ad58835f51301a1e5a82651dd570b8
shaun <shauncarpen@gmail.com>
2017-10-03 21:38:50 -0400

    implemented database connection

--- CSCI-320-KotDT/Controllers/LoginController.cs
+++ CSCI-320-KotDT/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Web.Mvc;
+using Npgsql;
 
 namespace CSCI_320_KotDT.Controllers {
     public class LoginController : Controller {
@@ -8,14 +10,29 @@ namespace CSCI_320_KotDT.Controllers {
         public ActionResult Login() {
             return View();
         }
-        
-        public ActionResult CreateUser() {
-            return View();
-        }
-
         [HttpPost]
         public ActionResult Login(string username, string password) {
             Console.WriteLine(username);
+            return RedirectToAction("Index", "Home");
+        }
+    
+        
+        [HttpGet]
+        public ActionResult CreateUser() {
+            ViewBag.invalid = false;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateUser(string username, string password, string firstName, string lastName) {
+            //check validity
+            using(var conn = new NpgsqlConnection("Host=reddwarf.cs.rit.edu; username=p32003f; password=kiexeiH7veiqu9Uta6Go")) 
+            {
+                conn.Open();
+            }
+            if(username.Equals("a")) {
+                ViewBag.invalid = true;
+                return View();
+            }
             return RedirectToAction("Index", "Home");
         }
     }
