ba007922aab50173198f15d1eeb9c32c86a23b91
Connor Mcauliffe <cxm4176@rit.edu>
2017-10-10 11:19:04 -0400

    savePoint

---
+++ CSCI-320-KotDT/Controllers/MoviesController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Mvc;
+using CSCI_320_KotDT.Models;
+using ControllerDI.Interfaces;
+
+namespace CSCI_320_KotDT.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly IQueryHandler QueryHandler;
+
+        private MovieDBContext db = new MovieDBContext();
+
+        public MoviesController(IQueryHandler IQuery)
+        {
+            QueryHandler = IQuery;
+        }
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            string queryString = "SELECT title, release_year, running_time FROM movies order by title limit 30";
+
+           
+
+            var cmd = QueryHandler.query(queryString);
+            List<Movie> movies = new List<Movie>();
+            using (var reader = cmd.ExecuteReader())
+            {
+                Console.Write(reader.GetName(0));
+                Console.Write(reader.GetName(1));
+                Console.Write(reader.GetName(2));
+                while (reader.Read())
+                {
+                    Console.Write(reader.FieldCount);
+
+                    String title = reader.GetString(0);
+                    Int32 ry = reader.GetInt32(1);
+                    Int32 rt = reader.GetInt32(2);
+                    Movie movie = new Movie(title, ry, rt);
+                    movies.Add(movie);
+
+                }
+            }
+            return View(movies);
+
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+             //   db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CSCI-320-KotDT/Models/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace CSCI_320_KotDT.Models
+{
+    [Table("movies")]
+    public class Movie
+    {
+        [Key]
+        public int MovieId{ get; set; }
+
+        public string Title { get; set; }
+
+        [Display(Name = "Release Year")]
+        public int Release_year { get; set; }
+        public int Running_time { get; set; }
+
+        public Movie(string Title)
+        {
+            this.Title = Title;
+        }
+
+        public Movie(string Title, int Release_year, int Running_time)
+        {
+            this.Title = Title;
+            this.Release_year = Release_year;
+            this.Running_time = Running_time;
+        }
+        
+
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
