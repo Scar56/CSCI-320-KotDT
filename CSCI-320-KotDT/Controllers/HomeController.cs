dbabbf17571d801416e559e86a1754c509895bd8
shaun <shauncarpen@gmail.com>
2017-10-13 18:55:13 -0400

    removed about and contact pages from home

--- CSCI-320-KotDT/Controllers/HomeController.cs
+++ CSCI-320-KotDT/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
@@ -19,15 +20,9 @@ namespace CSCI_320_KotDT.Controllers
 				System.Web.HttpContext.Current.Session["UserID"];
 			return View();
 		}
-		public ActionResult About() {
-			ViewBag.Message = "Your application description page.";
-			
-			return View();
-		}
-		
-		public ActionResult Contact() {
-			ViewBag.Message = "Your contact page.";
-			
+
+		[HttpPost]
+		public ActionResult Search(string search) {
 			return View();
 		}
 	}
