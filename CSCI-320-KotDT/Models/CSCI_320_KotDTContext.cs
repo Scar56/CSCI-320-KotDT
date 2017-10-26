128e1804fd8737114a4238c979e958c6c041a0ca
Michael Jones <michaelscottjones0@gmail.com>
2017-10-26 17:26:32 -0400

    quick fix

---
+++ CSCI-320-KotDT/Models/CSCI_320_KotDTContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CSCI_320_KotDT.Models
+{
+    public class CSCI_320_KotDTContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public CSCI_320_KotDTContext() : base("name=CSCI_320_KotDTContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<CSCI_320_KotDT.Models.Actor> Actors { get; set; }
+    }
+}
