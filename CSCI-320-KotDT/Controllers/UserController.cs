a5c1d4e58b28b918a580ff0e5961954355dd7cec
shaun <shauncarpen@gmail.com>
2017-11-02 15:50:13 -0400

    can unfollow users

--- CSCI-320-KotDT/Controllers/UserController.cs
+++ CSCI-320-KotDT/Controllers/UserController.cs
@@ -16,23 +16,51 @@ namespace CSCI_320_KotDT.Controllers {
         [HttpGet]
         public ActionResult User(string username) {
             string queryString = "Select username, first_name, last_name From \"User\" where username = '" + username + "'";
-            
+
             //should at most have one result
             ArrayList query = QueryHandler.read(queryString, 3);
-            System.Web.HttpContext.Current.Session["selectedUser"] = new User(username, (string) ((ArrayList)query[0])[1], (string) ((ArrayList)query[0])[2]);
+            System.Web.HttpContext.Current.Session["selectedUser"] = new User(username, (string) ((ArrayList) query[0])[1], (string) ((ArrayList) query[0])[2]);
+            ViewBag.followable = 0;
             
-            return View();
+            string currUser = null;
+            if(System.Web.HttpContext.Current.Session["UserID"] != null)
+                currUser = ((User) System.Web.HttpContext.Current.Session["UserID"]).username;
+            
+            if (currUser != null) {
+                ViewBag.followable = 1;
+                queryString = "Select follower, following From follows_user where follower = '" +
+                              ((User) System.Web.HttpContext.Current.Session["UserID"]).username + "'";
+    
+                query = QueryHandler.read(queryString, 2);
+                foreach (ArrayList i in query) {
+                    if(currUser.Equals(i[1]))
+                        ViewBag.followable = 2;
+                }
+                
+            }
+        
+
+        return View();
         }
 
         //adds usernames to follows_user
         public ActionResult follow() {
-            Console.WriteLine(((User)System.Web.HttpContext.Current.Session["UserID"]).username);
-            Console.WriteLine(((User)System.Web.HttpContext.Current.Session["selectedUser"]).username);
+            string currUser = ((User)System.Web.HttpContext.Current.Session["UserID"]).username;
+            string selUser = ((User)System.Web.HttpContext.Current.Session["selectedUser"]).username;
             string query = "Insert Into follows_user " +
                                "(follower, following) " + 
-                                    "VALUES('" + ((User)System.Web.HttpContext.Current.Session["UserID"]).username + "', '" + ((User)System.Web.HttpContext.Current.Session["selectedUser"]).username + "')";
+                                    "VALUES('" + currUser + "', '" + selUser + "')";
             QueryHandler.nonQuery(query);
-            return RedirectToAction("User", "User", new{username = ((User)System.Web.HttpContext.Current.Session["selectedUser"]).username});
+            return RedirectToAction("User", "User", new{username = selUser});
+        }
+        
+        public ActionResult unfollow() {
+            string currUser = ((User)System.Web.HttpContext.Current.Session["UserID"]).username;
+            string selUser = ((User)System.Web.HttpContext.Current.Session["selectedUser"]).username;
+            string query = "delete from follows_user " +
+                           "where(follower = '" + currUser + "' AND  following = '" + selUser + "')";
+            QueryHandler.nonQuery(query);
+            return RedirectToAction("User", "User", new{username = selUser});
         }
     }
 }
