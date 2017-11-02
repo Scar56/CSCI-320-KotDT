58225026986071acbffeba1892d8a007cc802623
shaun <shauncarpen@gmail.com>
2017-11-02 14:59:19 -0400

    can follow users

--- CSCI-320-KotDT/Controllers/UserController.cs
+++ CSCI-320-KotDT/Controllers/UserController.cs
@@ -19,10 +19,20 @@ namespace CSCI_320_KotDT.Controllers {
             
             //should at most have one result
             ArrayList query = QueryHandler.read(queryString, 3);
-            Console.WriteLine(username);
             System.Web.HttpContext.Current.Session["selectedUser"] = new User(username, (string) ((ArrayList)query[0])[1], (string) ((ArrayList)query[0])[2]);
             
             return View();
+        }
+
+        //adds usernames to follows_user
+        public ActionResult follow() {
+            Console.WriteLine(((User)System.Web.HttpContext.Current.Session["UserID"]).username);
+            Console.WriteLine(((User)System.Web.HttpContext.Current.Session["selectedUser"]).username);
+            string query = "Insert Into follows_user " +
+                               "(follower, following) " + 
+                                    "VALUES('" + ((User)System.Web.HttpContext.Current.Session["UserID"]).username + "', '" + ((User)System.Web.HttpContext.Current.Session["selectedUser"]).username + "')";
+            QueryHandler.nonQuery(query);
+            return RedirectToAction("User", "User", new{username = ((User)System.Web.HttpContext.Current.Session["selectedUser"]).username});
         }
     }
 }
