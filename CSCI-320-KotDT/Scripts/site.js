da7bba0776bb58f765b9a230474ea12fd84905f0
connormcauliffe <cxm4176@rit.edu>
2017-11-30 10:27:50 -0500

    added a way to hide the huge lists of actors

--- CSCI-320-KotDT/Scripts/site.js
+++ CSCI-320-KotDT/Scripts/site.js
@@ -1,1 +1,16 @@
 // Write your Javascript code.
+
+function toggleCastList(){
+    console.log(document.getElementById('castList'))
+    var table = document.getElementById("castList");
+    for (var i = 10, row; row = table.rows[i]; i++) { 
+
+
+        if (row.style.display === "table-row") {
+            row.style.display = "none";
+        } else {
+            row.style.display = "table-row";
+        }
+         
+    } 
+}
